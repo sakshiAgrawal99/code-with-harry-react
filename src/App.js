69cd4d8be71df2376e0cae6bb8a43cadfbc87978
Sakshi Agrawal <sakshi.agrawal@newvisionsoftpune.com>
2023-02-15 18:02:52 +0530

    video 8

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
+import About from "./components/About";
 
 function App() {
   return (
     <>
       <Navbar title="TextUtils" />
       <div className="container my-3">
-        <TextForm heading="Enter text to analyze below"></TextForm>
+        {/* <TextForm heading="Enter text to analyze below"></TextForm> */}
+        <About />
       </div>
     </>
   );
